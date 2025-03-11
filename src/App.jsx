f556e197e3813f93fee8fcfb5815f6654d30935f
rawanmohameedd <nrawana12345@gmail.com>
2025-03-11 13:58:04 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import { Route, Routes } from 'react-router-dom';
-import { Aboutme } from './Components/Aboutme';
-import Contactme from './Components/contactme';
-import { Projects } from './Components/Projects';
-import Experience from './Components/Experience';
-import { Sidebar } from './Components/sidebar';
-import './App.css';
+import { Route, Routes, useLocation } from "react-router-dom";
+import { Aboutme } from "./Components/Aboutme";
+import Contactme from "./Components/contactme";
+import { Projects } from "./Components/Projects";
+import Experience from "./Components/Experience";
+import { Sidebar } from "./Components/sidebar";
+import { useEffect } from "react";
+import "./App.css";
+import "./transitions.css"; 
 
 export default function App() {
+  const location = useLocation();
+
+  useEffect(() => {}, [location]);
+
   return (
     <div className="flex min-h-screen">
       {/* Sidebar */}
@@ -14,14 +20,18 @@ export default function App() {
         <Sidebar />
       </div>
 
-      {/* Main Content */}
+      {/* Main Content with Transition */}
       <div className="flex flex-col w-[75%] ml-[25%] justify-center items-center">
-        <Routes>
-          <Route path="/" element={<Aboutme />} />
-          <Route path="/contactme" element={<Contactme />} />
-          <Route path="/experience" element={<Experience />} />
-          <Route path="/projects" element={<Projects />} />
-        </Routes>
+        <div className="w-full h-full transition-container">
+          <div className="fade">
+            <Routes location={location} key={location.pathname}>
+              <Route path="/" element={<Aboutme />} />
+              <Route path="/contactme" element={<Contactme />} />
+              <Route path="/experience" element={<Experience />} />
+              <Route path="/projects" element={<Projects />} />
+            </Routes>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/Components/Aboutme.jsx
+++ src/Components/Aboutme.jsx
@@ -68,7 +68,7 @@ export function Aboutme() {
                     </p>
                     <a
                         className="inline-block text-primary font-semibold underline mt-2 text-lg hover:text-blue-600 transition"
-                        href="https://drive.google.com/file/d/1KP3Zja62gV4-MX81dwylVCFn8irtcwqD/view?usp=sharing"
+                        href="https://drive.google.com/file/d/13FMTcbzcDTrUcpNXopEcexo6kcjlnUTi/view?usp=sharing"
                         target="_blank"
                         rel="noopener noreferrer"
                     >
